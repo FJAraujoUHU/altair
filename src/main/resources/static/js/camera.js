0db0df21a640e35271971517a77f005f3fb734ff
Francisco Javier Araújo Mendoza <franciscojavier.araujo885@alu.uhu.es>
2023-04-26 18:36:41 +0200

    ref(camera): camera frontend V1 done

--- src/main/resources/static/js/camera.js
+++ src/main/resources/static/js/camera.js
@@ -1,4 +1,4 @@
-let connected, temperature, cameraStatus, binning, subframe, cooler;
+let connected, temperature, cameraStatus, binning, subframe, cooler, coolerConnected;
 $(document).ready(function () {
     // Set up CSRF token
     let token = $("meta[name='_csrf']").attr("content");
@@ -33,14 +33,32 @@ $(document).ready(function () {
         $("#caBinning").text(binning);
         $("#caSubframe").text(subframe);
         $("#caCooler").text(cooler);
+        $("#caCoolerPwr").text(data.coolerPower);
 
-        // TODO : update controls
+        // TODO : dont use placeholder
+        connected = true; // spoof simulator, must remove for production
         if (connected) {
             $("#caConnect").text("Disconnect");
         } else {
             $("#caConnect").text("Connect");
         }
+        if (data.coolerStatus.toUpperCase() !== "OFF") {
+            coolerConnected = true;
+            $("#caCoolerEnable").text("Turn off");
+        } else {
+            coolerConnected = false;
+            $("#caCoolerEnable").text("Turn on");
+        }
         
+        $("#caSubframeButton").prop('disabled', !connected);
+        $("#caBinningSym").prop('disabled', !connected);
+        $("#caBinningSym").prop('disabled', !connected);
+        $("#caExposureBtns").find('button').prop('disabled', !connected);
+        $("#caExposureDLBtns").find('button').prop('disabled', !connected);
+        $("#caCoolerEnable").prop('disabled', !connected);
+        $("#caCoolerWarmup").prop('disabled', !connected);
+        $("#caCoolerTempSetPane").find('button').prop('disabled', !connected);
+
     }
 
     // Set up controls
@@ -55,4 +73,173 @@ $(document).ready(function () {
             }
         });
     });
+
+    $("#caSubframeButton").click(function () {
+        let startx = $("#caSubframeStartX").val();
+        let starty = $("#caSubframeStartY").val();
+        let width = $("#caSubframeWidth").val();
+        let height = $("#caSubframeHeight").val();
+
+        $.ajax({
+            url: "/altair/api/camera/setsubframe",
+            type: "POST",
+            data: {
+                startx: startx,
+                starty: starty,
+                width: width,
+                height: height
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+    // TODO : Symmetric does nothing for now
+    $("#caBinningSet").click(function () {
+        let binx = $("#caBinningX").val();
+        let biny = $("#caBinningY").val();
+
+        $.ajax({
+            url: "/altair/api/camera/setbinning",
+            type: "POST",
+            data: {
+                binx: binx,
+                biny: biny
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+    
+    $("#caExposureStart").click(function () {
+        let hours = $("#caExposureHours").val();
+        let minutes = $("#caExposureMinutes").val();
+        let seconds = $("#caExposureSeconds").val();
+        let duration = (hours * 3600) + (minutes * 60) + seconds;
+        let lightframe = $("#caExposureLight").is(":checked");
+
+        $.ajax({
+            url: "/altair/api/camera/startexposure",
+            type: "POST",
+            data: {
+                duration: duration,
+                lightframe: lightframe
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+    $("#caExposureStop").click(function () {
+        $.ajax({
+            url: "/altair/api/camera/stopexposure",
+            type: "POST",
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+    $("#caExposureAbort").click(function () {
+        $.ajax({
+            url: "/altair/api/camera/abortexposure",
+            type: "POST",
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+    // TODO : Prompt for filename and check if there are available first
+    $("#caImgSave").click(function () {
+        let filename = new Date().toISOString();
+
+        $.ajax({
+            url: "/altair/api/camera/saveimage",
+            type: "POST",
+            data: {
+                filename: filename
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+    $("#caImgDump").click(function () {
+        let filename = new Date().toISOString();
+
+        $.ajax({
+            url: "/altair/api/camera/dumpimage",
+            type: "POST",
+            data: {
+                filename: filename
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+    $("#caCoolerEnable").click(function () {
+        let enable = !coolerConnected;
+
+        $.ajax({
+            url: "/altair/api/camera/cooleron",
+            type: "POST",
+            data: {
+                enable: enable
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+    $("#caCoolerWarmup").click(function () {
+        $.ajax({
+            url: "/altair/api/camera/coolwarmup",
+            type: "POST",
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+
+    $("#caCoolerTargetEase").click(function () {
+        let target = $("#caCoolerTarget").val();
+
+        $.ajax({
+            url: "/altair/api/camera/cooldown",
+            type: "POST",
+            data: {
+                target: target
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+    
+    $("#caCoolerTargetSet").click(function () {
+        let target = $("#caCoolerTarget").val();
+
+        $.ajax({
+            url: "/altair/api/camera/settargettemp",
+            type: "POST",
+            data: {
+                target: target
+            },
+            error: function (xhr, status, error) {
+                console.log("Error: " + error);
+            }
+        });
+    });
+
+
+
 });
